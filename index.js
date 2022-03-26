bd1c2b74d2a8da63a67983b3525abd88e863ac0b
thuitaf <faith.thuita@moringaschool.com>
2022-03-26 17:40:51 +0300

    WIP

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Your code here
+function createEmployeeRecord(record){
+    return {
+        firstName:record[0],
+        familyName:record[1],
+        title:record[2],
+        payPerHour:record[3],
+        timeInEvents:[],
+        timeOutEvents:[]
+    }
+    
+
+}
+function createEmployeeRecords(employeeData){
+    return employeeData.map(function(record){
+        return createEmployeeRecord(record)
+
+    })
+
+}
+function createTimeInEvent(employee,date){
+
+    
+
+
+}
